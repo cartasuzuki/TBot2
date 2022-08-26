7394d1c9c43f5974ae77314219abb3c74fed4405
cartasuzuki <46107174+cartasuzuki@users.noreply.github.com>
2022-08-26 17:58:32 +0900

    Add files via upload

--- Arduino/BraccioFirmataInverseKinematics/BraccioFirmataInverseKinematics.ino
+++ Arduino/BraccioFirmataInverseKinematics/BraccioFirmataInverseKinematics.ino
@@ -31,12 +31,18 @@ Servo wrist_rot;
 Servo wrist_ver;
 Servo gripper;
 
+//Current position of the Braccio Arm in mm
+float x, y, z; 
+
+//Braccio Servos motors angle position
+float m1, m2, m3, m4,m5, m6;
+
 Link baseLink, upperarmLink, forearmLink, handLink;
 
 
 void PerformInverseKinematics(float x,float y,float z)
 { 
-  float m1, m2, m3, m4,m5, m6, a1, a2, a3, a4;
+  float a1, a2, a3, a4;
   lcd.clear();
   lcd.setCursor(1,0);
 
@@ -51,13 +57,12 @@ void PerformInverseKinematics(float x,float y,float z)
   m2 = a2b(a2);
   m3 = a2b(a3);  
   m4 = a2b(a4);
-  m5 = 90;
-  m6 = 40; 
+
 
                        //(step delay  M1 , M2 , M3 , M4 , M5 , M6);
   Braccio.ServoMovement(20,           m1, m2,  m3,  m4,  m5,  m6);
 
-    //Wait 5 second
+    //Wait 3 second
   delay(3000);
   
 }
@@ -65,48 +70,81 @@ void PerformInverseKinematics(float x,float y,float z)
 
 void stringCallback(char *msg)
 { 
-  float x, y, z;
   char command;
-  x = 0;
-  y = 0;
-  z = 0;
+
   lcd.clear();
   lcd.setCursor(1,0);
   lcd.print(msg);
-  delay(10);
+  delay(2);
   lcd.clear();
 
   String data = String(msg);
 
   command = data.charAt(0);
   data.remove(0,1);
-  
+
   switch (command)
   {
-    case 'x':
+
+   case 'x':
+   {
       x = data.toFloat();
       String inputx = "x = " + String(x);
+      lcd.setCursor(2,0);
       lcd.print(inputx);
       break;
-
-     case 'y':
+    }
+     
+    case 'y':
+    {
       y = data.toFloat();
       String inputy = "y = " + String(y);
+      lcd.setCursor(2,0);
       lcd.print(inputy);
       break;
+    }
 
      case 'z':
+     {
       z = data.toFloat();
       String inputz = "z = " + String(z);
       lcd.print(inputz);
       break;
+     }
 
-     case 'p':
+     case 'c':
+     {
       PerformInverseKinematics(x,y,z);
       break;
+     }
 
+     case 'g':
+     {
+      lcd.print("Gripper");
+      // Open/Close gripper
+      m6 = data.toFloat();
+      
+      // Only move gripper
+                      //(step delay  M1 , M2 , M3 , M4 , M5 , M6);
+      Braccio.ServoMovement(20,       m1, m2,  m3,  m4,  m5,  m6);
+      
+      break;
+     }
+
+     case 'p':
+     {
+      lcd.clear();
+      lcd.setCursor(1,0);
+      lcd.print("x y z " + String(x));
+      lcd.setCursor(2,0);
+      lcd.print(String(y) + " " + String(z));
+      break;
+     }
+      
      default:
+     {
       lcd.print("Error");
+     }
   }
   
   
@@ -116,7 +154,7 @@ void stringCallback(char *msg)
 
 void systemResetCallback()
 {
-  
+  Braccio.begin();
 }
 
 void setup()
@@ -129,19 +167,20 @@ void setup()
   Firmata.begin(57600);
   systemResetCallback();
 
+  // Initiate the InverseK library for the Braccio Arm
   baseLink.init(74, b2a(0.0), b2a(180.0));
   upperarmLink.init(125, b2a(15.0), b2a(165.0));
   forearmLink.init(125, b2a(0.0), b2a(180.0));
   handLink.init(195, b2a(0.0), b2a(180.0));
   InverseK.attach(baseLink, upperarmLink, forearmLink, handLink);
 
+  x = 0;
+  y = 0;
+  z = 0;
   lcd.init();
   // Print a message to the LCD.
+  lcd.clear();
   lcd.backlight();
-  lcd.setCursor(1,0);
-  lcd.print("Testing");
-  lcd.setCursor(1,1);
-  lcd.print("PyFirmata");
 
   //Initialization functions and set up the initial position for Braccio
   //All the servo motors will be positioned in the "safety" position:
@@ -151,8 +190,8 @@ void setup()
   //Wrist vertical (M4): 180 degrees
   //Wrist rotation (M5): 90 degrees
   //gripper (M6): 10 degrees
-  Braccio.begin();
-  lcd.clear();
+  Braccio.begin(); 
+  
   lcd.print("Braccio Ready");
   
 
